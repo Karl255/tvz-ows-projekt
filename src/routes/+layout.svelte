d15f36f114584cc1eae88d09b16f94f0609c08e4
Karl255 <49595438+Karl255@users.noreply.github.com>
2024-01-06 12:22:39 +0100

    Added logo, favicon, adjusted header and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,10 @@
 
 <header>
 	<div class="container header__content">
-		<span class="temp">LOGO</span>
+		<a href="/">
+			<!-- prettier-ignore -->
+			<img src="/logo.png" alt="logo">
+		</a>
 
 		<nav>
 			<!-- prettier-ignore -->
@@ -18,14 +21,15 @@
 		</nav>
 
 		<div class="header__icons">
-			<a href=".">
-				<!-- prettier-ignore -->
+			<!-- prettier-ignore -->
+			<a href="." class="icon">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
 					<path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
 				  </svg>
 			</a>
-			<a href=".">
-				<!-- prettier-ignore -->
+
+			<!-- prettier-ignore -->
+			<a href="." class="icon">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
 					<path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
 				</svg>
@@ -42,8 +46,8 @@
 	<div class="container">
 		<!-- prettier-ignore -->
 		<ul class="nav-links">
+			<li><a class="link" href=".">Dostava</a></li>
 			<li><a class="link" href=".">O nama</a></li>
-			<li><a class="link" href=".">Misija</a></li>
 			<li><a class="link" href=".">Kontakt</a></li>
 		</ul>
 	</div>
@@ -91,14 +95,14 @@
 	}
 
 	header {
-		background-color: #eee;
+		padding: 1.5rem;
 	}
 
 	.header__content {
 		display: grid;
-		grid-template-columns: 4.5rem 1fr 4.5rem;
+		grid-template-columns: 6rem 1fr 6rem;
 		align-items: center;
-		gap: 2rem;
+		gap: 4rem;
 	}
 
 	.header__icons {
@@ -114,7 +118,7 @@
 	}
 
 	footer {
-		background-color: #eee;
+		padding: 1rem;
 	}
 
 	.container {
@@ -137,14 +141,23 @@
 	a {
 		display: block;
 		color: inherit;
-		text-decoration: none; // TODO: maybe remove?
 	}
 
 	a.link {
-		padding: 1rem;
+		text-decoration-line: underline;
+		text-decoration-color: transparent;
+		transition: text-decoration-color 50ms ease-out;
 
 		&:hover {
-			background-color: #ddd;
+			text-decoration-color: currentColor;
+		}
+	}
+
+	a.icon {
+		transition: color 100ms ease-out;
+
+		&:hover {
+			color: #555;
 		}
 	}
 
